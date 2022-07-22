f4ea96db147fccb9a4b5ef231d6d0bba53420b5a
Gabriel Victor <gabriel.victor@liferay.com>
2022-07-22 14:40:51 -0300

    starting project

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import { Button } from './components /Button'
+
+
+
+export function App() {
+
+  return (
+    <>
+      <Button variant='primary'/>
+      <Button variant='secondary'/>
+      <Button variant='success'/>
+      <Button variant='danger'/>
+    </>
+    
+  )
+}
+
---
+++ src/components /Button.tsx
@@ -0,0 +1,10 @@
+import { ButtonContainer, ButtonVariant } from "./Button.styles";
+
+
+interface ButtonPrpos {
+    variant?: ButtonVariant;
+}
+
+export function Button({ variant = 'primary'}:ButtonPrpos){
+    return<ButtonContainer variant={variant} >Enviar</ButtonContainer>
+}
