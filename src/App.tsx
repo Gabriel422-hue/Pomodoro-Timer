8a7eafd16a0f74e85469a36140dacc745a6a021b
Gabriel Victor <gabriel.victor@liferay.com>
2022-07-22 15:40:10 -0300

    theme typing & Globais Styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,22 @@
-import { useState } from 'react'
+import { ThemeProvider } from 'styled-components'
 import { Button } from './components /Button'
+import { GlobalStyles } from './styles/global'
+import { defaultTheme } from './styles/themes/default'
 
 
 
 export function App() {
 
   return (
-    <>
+    <ThemeProvider theme={defaultTheme}>
       <Button variant='primary'/>
       <Button variant='secondary'/>
       <Button variant='success'/>
       <Button variant='danger'/>
-    </>
+
+
+      <GlobalStyles/>
+    </ThemeProvider>
     
   )
 }
--- src/components /Button.styles.ts
+++ src/components /Button.styles.ts
@@ -17,10 +17,9 @@ export const ButtonContainer = styled.button<ButtonContainerProps>`
 width:100px;
 height: 100px; 
 
-${props => {
-    return css`
-        background-color: ${buttonVariants[props.variant]}`
-}}
+background-color: ${props =>props.theme.primary};
+
+
 
 `
 
