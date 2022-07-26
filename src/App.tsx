8e0eb0d916c308323ad6ff440f961922ecb50cc1
Gabriel Victor <gabriel.victor@liferay.com>
2022-07-26 13:40:36 -0300

    Configurando React Router Dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,15 @@
 import { ThemeProvider } from 'styled-components'
-import { Button } from './components /Button'
+import { Router } from './Router'
+import { BrowserRouter } from 'react-router-dom'
 import { GlobalStyles } from './styles/global'
 import { defaultTheme } from './styles/themes/default'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Button variant="primary" />
-      <Button variant="secondary" />
-      <Button variant="success" />
-      <Button variant="danger" />
-
+        <BrowserRouter>
+          <Router/>
+        </BrowserRouter>
       <GlobalStyles />
     </ThemeProvider>
   )
--- src/components /Button.tsx
+++
@@ -1,9 +0,0 @@
-import { ButtonContainer, ButtonVariant } from './Button.styles'
-
-interface ButtonPrpos {
-  variant?: ButtonVariant
-}
-
-export function Button({ variant = 'primary' }: ButtonPrpos) {
-  return <ButtonContainer variant={variant}>Enviar</ButtonContainer>
-}
